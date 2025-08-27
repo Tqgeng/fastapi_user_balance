fbd0936d670c57ac28ee496ed62b5704247f7cf4
Tqgeng <pavel.kramarew@mail.ru>
2025-08-27 13:51:38 +0300

    add get all users request

--- fastapi-application/api/api_v1/users.py
+++ fastapi-application/api/api_v1/users.py
@@ -1,11 +1,14 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
 
+from core.models import db_helper
 from core.shemas.user import (
     UserRead,
     UserUpdate,
 )
 from .fastapi_users_router import fastapi_users
 from core.config import settings
+from crud.users import get_all_users
 
 router = APIRouter(
     prefix=settings.api.v1.users,
@@ -19,3 +22,9 @@ router.include_router(
         UserUpdate,
     ),
 )
+
+
+@router.get("", response_model=list[UserRead])
+async def get_all(session: AsyncSession = Depends(db_helper.session_getter)):
+    users = await get_all_users(session)
+    return users
